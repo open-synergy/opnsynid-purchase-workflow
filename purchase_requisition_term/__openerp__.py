293729589a1121fb8e800c890a899825380c6fa5
Andhitia Rama <andhitia.r@gmail.com>
2016-04-28 05:29:15 +0700

    [ADD] purchase_requisition_term

---
+++ purchase_requisition_term/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Requisition Term",
+    "summary": "Add terms to purchase requisition",
+    "version": "8.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase_requisition",
+        "purchase_order_term",
+    ],
+    "data": [
+        "views/purchase_requisition_views.xml",
+    ],
+}
---
+++ purchase_requisition_term/models/purchase_requisition.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class PurchaseRequisitionLine(models.Model):
+    _inherit = "purchase.requisition.line"
+
+
+
+class PurchaseRequisition(models.Model):
+    _inherit = "purchase.requisition"
+
+    term_ids = fields.Many2many(
+        string="Term(s)",
+        comodel_name="purchase.term",
+        column1="pr_id",
+        column2="term_id",
+    )
+
+    @api.model
+    def _prepare_purchase_order(self, requisition, supplier):
+        res = super(PurchaseRequisition, self)._prepare_purchase_order(
+            requisition, supplier)
+
+        if requisition.term_ids:
+            term_ids = []
+            for term in requisition.term_ids:
+                term_ids.append(term.id)
+            res.update({'term_ids': [(6, 0, term_ids)]})
+
+        return res
