06e71d168257b6800cc9c2208a16305db2b77c0a
mike <viriyananda.michael@gmail.com>
2017-03-14 17:35:23 +0700

    Adding new module purchase_order_type_extension

---
+++ purchase_order_type_extension/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Type Extension",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "purchase_order_type",
+    ],
+    "data": [
+        "views/purchase_order_type_views.xml",
+    ],
+}
