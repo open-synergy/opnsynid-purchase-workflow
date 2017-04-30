8faebe9c7061e10a48214de36dcc36b3282cf7f9
Andhitia Rama <andhitia.r@gmail.com>
2017-04-30 08:13:16 +0700

    [ADD][8.0] purchase_request_to_rfq_product_policy

---
+++ purchase_request_to_rfq_product_policy/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Request to RFQ Product Policy",
+    "version": "8.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "purchase_request_product_policy",
+        "purchase_request_type_to_rfq",
+    ],
+    "data": [
+        "wizard/purchase_request_line_make_purchase_order_view.xml",
+    ],
+}
