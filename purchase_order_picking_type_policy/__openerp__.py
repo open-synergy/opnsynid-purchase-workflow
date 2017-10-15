5452f1aabfd0f92d021c439af7546adadb416963
Andhitia Rama <andhitia.r@gmail.com>
2017-10-15 17:17:05 +0700

    [8.0.2.0.0] purchase_order_picking_type_policy
    * Add dependency to purchase_order_policy
    * Form content move to notebook

--- purchase_order_picking_type_policy/__openerp__.py
+++ purchase_order_picking_type_policy/__openerp__.py
@@ -3,14 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Purchase Order Picking Type Policy",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.2.0",
     "category": "Purchase",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "purchase_order_type_extension",
+        "purchase_order_policy",
     ],
     "data": [
         "views/purchase_order_type_views.xml",
