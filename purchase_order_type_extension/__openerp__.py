0733304088a7c7d0a9a60d2a9050d42027817d8c
Andhitia Rama <andhitia.r@gmail.com>
2017-10-15 17:21:36 +0700

    [8.0.1.1.0] purchase_order_type_extension
    * Form content move to notebook

--- purchase_order_type_extension/__openerp__.py
+++ purchase_order_type_extension/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Order Type Extension",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
