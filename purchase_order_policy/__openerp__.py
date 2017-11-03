2897bde852811f39a7f9ebd667ae15e78ec0796f
Andhitia Rama <andhitia.r@gmail.com>
2017-11-03 11:55:46 +0700

    [8.0.2.0.4] purchase_order_policy

--- purchase_order_policy/__openerp__.py
+++ purchase_order_policy/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Order Policy",
-    "version": "8.0.2.0.3",
+    "version": "8.0.2.0.4",
     "category": "Purchase Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
