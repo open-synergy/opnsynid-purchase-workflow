1ca10b3506e06841e098366bd960b92b7bf72b36
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-19 08:09:00 +0700

    [8.0.1.1.0][purchase_request_product_policy]Issue: #55

--- purchase_request_product_policy/models/purchase_request.py
+++ purchase_request_product_policy/models/purchase_request.py
@@ -8,25 +8,31 @@ from openerp import models, fields, api
 class PurchaseRequest(models.Model):
     _inherit = "purchase.request"
 
+    @api.multi
     @api.depends(
-        "order_type_id",
-        "order_type_id.all_allowed_product_ids")
-    def _compute_all_allowed_product_ids(self):
+        "order_type_id"
+    )
+    def _compute_allowed_product(self):
         obj_product = self.env["product.product"]
-        for pr in self:
-            if pr.order_type_id.limit_product_selection:
-                pr.all_allowed_product_ids = \
-                    pr.order_type_id.all_allowed_product_ids
+        for document in self:
+            if document.order_type_id.limit_product_selection:
+                document.allowed_product_ids = \
+                    document.order_type_id.allowed_product_ids.ids
+                document.allowed_product_categ_ids = \
+                    document.order_type_id.allowed_product_categ_ids.ids
             else:
-                criteria = [
-                    ("purchase_ok", "=", True),
-                ]
-                pr.all_allowed_product_ids = \
-                    obj_product.search(criteria)
+                document.allowed_product_ids = \
+                    obj_product.search([("purchase_ok", "=", True)])
 
-    all_allowed_product_ids = fields.Many2many(
-        string="All Allowed Product",
+    allowed_product_ids = fields.Many2many(
+        string="Allowed Products",
         comodel_name="product.product",
-        compute="_compute_all_allowed_product_ids",
+        compute="_compute_allowed_product",
+        store=False,
+    )
+    allowed_product_categ_ids = fields.Many2many(
+        string="Allowed Product Categories",
+        comodel_name="product.category",
+        compute="_compute_allowed_product",
         store=False,
     )
