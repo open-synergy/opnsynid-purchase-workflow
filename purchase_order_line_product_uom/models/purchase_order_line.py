329c4ff9e0919958837ff1c642751ba3c12a0406
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-21 09:43:04 +0700

    [8.0.1.2.0][purchase_order_line_product_uom]Adding group products.group_uom

--- purchase_order_line_product_uom/models/purchase_order_line.py
+++ purchase_order_line_product_uom/models/purchase_order_line.py
@@ -35,11 +35,7 @@ class PurchaseOrderLine(models.Model):
     def _compute_product_uom_qty(self):
         for document in self:
             document.product_uom_qty = 0.0
-            product_uom = document.product_uom_id
-            line_uom = document.product_uom
-            if product_uom and \
-                    line_uom and \
-                    product_uom.category_id.id == line_uom.category_id.id:
+            if document.product_uom_id:
                 document.product_uom_qty =\
                     document.product_uom_id._compute_qty(
                         from_uom_id=document.product_uom.id,
