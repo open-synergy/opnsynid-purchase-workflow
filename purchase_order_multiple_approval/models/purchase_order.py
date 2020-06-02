6e1a9c2e80ebc10a07d895ed7b7e655becd33b65
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-02 11:22:52 +0700

    [8.0.1.0.0] purchase_order_multiple_approval

---
+++ purchase_order_multiple_approval/models/purchase_order.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openerp import models, fields, api
+
+
+class PurchaseOrder(models.Model):
+    _name = "purchase.order"
+    _inherit = [
+        "purchase.order",
+        "tier.validation",
+    ]
+    _state_from = [
+        "draft",
+        "confirmed"
+    ]
+    _state_to = [
+        "approved",
+    ]
+
+    STATE_SELECTION = [
+        ("draft", "Draft PO"),
+        ("sent", "RFQ"),
+        ("bid", "Bid Received"),
+        ("confirmed", "Waiting for Approval"),
+        ("approved", "Purchase Approved"),
+        ("except_picking", "Shipping Exception"),
+        ("except_invoice", "Invoice Exception"),
+        ("done", "Done"),
+        ("cancel", "Cancelled")
+    ]
+
+    state = fields.Selection(
+        string="Status",
+        selection=STATE_SELECTION,
+        readonly=True,
+        select=True,
+        copy=False,
+    )
+
+    @api.multi
+    def wkf_confirm_order(self):
+        _super = super(PurchaseOrder, self)
+        _super.wkf_confirm_order()
+        for document in self:
+            document.request_validation()
+
+    @api.multi
+    def wkf_action_cancel(self):
+        _super = super(PurchaseOrder, self)
+        _super.wkf_action_cancel()
+        for document in self:
+            document.restart_validation()
+
+    @api.multi
+    def validate_tier(self):
+        _super = super(PurchaseOrder, self)
+        _super.validate_tier()
+        for document in self:
+            if document.validated:
+                document.wkf_approve_order()
+
+    @api.multi
+    def restart_validation(self):
+        _super = super(PurchaseOrder, self)
+        _super.restart_validation()
+        for document in self:
+            document.request_validation()
