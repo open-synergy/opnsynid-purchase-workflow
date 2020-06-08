bd3ddd330c70726e177ea938a41fe27c84daf4a0
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-08 10:46:34 +0700

    [8.0.1.0.0][purchase_request_multiple_approval] Revision

--- purchase_request_multiple_approval/models/purchase_request.py
+++ purchase_request_multiple_approval/models/purchase_request.py
@@ -2,7 +2,7 @@
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from openerp import models
+from openerp import models, api
 
 
 class PurchaseRequest(models.Model):
@@ -11,5 +11,39 @@ class PurchaseRequest(models.Model):
         "purchase.request",
         "tier.validation",
     ]
-    _state_from = ["to_approve"]
-    _state_to = ["approved"]
+    _state_from = [
+        "draft",
+        "to_approve"
+    ]
+    _state_to = [
+        "approved",
+    ]
+
+    @api.multi
+    def button_to_approve(self):
+        _super = super(PurchaseRequest, self)
+        _super.button_to_approve()
+        for document in self:
+            document.request_validation()
+
+    @api.multi
+    def button_rejected(self):
+        _super = super(PurchaseRequest, self)
+        _super.button_rejected()
+        for document in self:
+            document.restart_validation()
+
+    @api.multi
+    def validate_tier(self):
+        _super = super(PurchaseRequest, self)
+        _super.validate_tier()
+        for document in self:
+            if document.validated:
+                document.button_approved()
+
+    @api.multi
+    def restart_validation(self):
+        _super = super(PurchaseRequest, self)
+        _super.restart_validation()
+        for document in self:
+            document.request_validation()
